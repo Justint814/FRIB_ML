6028827b35c9959a772d9c7d9f0614103f36fc67
Justint814 <168588108+Justint814@users.noreply.github.com>
2025-04-13 20:11:01 -0500

    Update setup.py

--- ML_pack/setup.py
+++ ML_pack/setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-name = 'ML_package',
-packages = ['ML_package'],
+name = 'FRIB_ML',
+packages = ['FRIB_ML'],
 version = '0.1',
 license = 'MIT',
 description = 'This package is used for creating training data and training a machine learning model for the FRIB level scheme project',
 author = 'Justin Traywick',
 author_email = 'jtraywick8@gmail.com',
-url = 'https://github.com/Justint814/ML_Package',
-download_url = 'https://github.com/Justint814/ML_Package/archive/refs/tags/code.tar.gz',
+url = 'https://github.com/Justint814/FRIB_ML',
+download_url = 'https://github.com/Justint814/FRIB_ML/archive/refs/tags/code.tar.gz',
 keywords = ['Machine', 'Learning', 'ML', 'train', 'data', 'level'],
 install_requires = ['numpy', 'tensorflow', 'os', 'random', 'h5py', 'math', 'shutil', 'subprocess', 'skimage'],
 classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'Topic :: Software Development :: Build Tools', 'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3', 'Programming Language :: Python :: 3.4', 'Programming Language :: Python :: 3.5', 'Programming Language :: Python :: 3.6',],
