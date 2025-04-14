1a2cdc44f98762c6f5fd2dc81b840bfbd200429d
Justint814 <168588108+Justint814@users.noreply.github.com>
2025-04-13 22:53:58 -0500

    Update ML.py

--- ML_pack/FRIB_ML/ML.py
+++ ML_pack/FRIB_ML/ML.py
@@ -16,12 +16,12 @@ import FRIB_ML as ML
 def h5_process(file_name):
 	with h5py.File(file_name, 'r') as f:
 		dataset = list(f.keys())[0]
-		a = np.array([dataset])
+		a = np.array(f[dataset])
 		#Process array
-		array = skimage.transform.downscale_local_mean(a,(1,5,5))
+		array = skimage.transform.downscale_local_mean(a,(5,5))
 		f.close()
 		
-		return array
+	return array
 	
 #Python generator zips together data and label images. Takes two numpy arrays of h5 file names
 def data_generator(h5_data,h5_labels):
